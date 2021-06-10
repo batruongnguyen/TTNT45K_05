a5227b43f815fa245d629c06a2946bdc1859c242
ngoctrinh020501 <ngoctrinh020501@gmail.com>
2021-06-10 10:20:08 +0700

    Calculator

---
+++ frmCalculator/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace frmCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.txtSo2 = new System.Windows.Forms.TextBox();
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(116, 231);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(75, 23);
+            this.btnCong.TabIndex = 0;
+            this.btnCong.Text = "Cộng";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // btnTru
+            // 
+            this.btnTru.Location = new System.Drawing.Point(278, 231);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(75, 23);
+            this.btnTru.TabIndex = 1;
+            this.btnTru.Text = "Trừ";
+            this.btnTru.UseVisualStyleBackColor = true;
+            this.btnTru.Click += new System.EventHandler(this.btnTru_Click);
+            // 
+            // txtSo2
+            // 
+            this.txtSo2.Location = new System.Drawing.Point(516, 71);
+            this.txtSo2.Name = "txtSo2";
+            this.txtSo2.Size = new System.Drawing.Size(100, 23);
+            this.txtSo2.TabIndex = 2;
+            this.txtSo2.Tag = "2";
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Location = new System.Drawing.Point(149, 71);
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(100, 23);
+            this.txtSo1.TabIndex = 1;
+            this.txtSo1.Tag = "1";
+            this.txtSo1.TextChanged += new System.EventHandler(this.txtSo1_TextChanged);
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.ForeColor = System.Drawing.Color.Red;
+            this.txtKQ.Location = new System.Drawing.Point(337, 146);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(100, 23);
+            this.txtKQ.TabIndex = 5;
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Location = new System.Drawing.Point(442, 231);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(75, 23);
+            this.btnNhan.TabIndex = 6;
+            this.btnNhan.Text = "Nhân";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            this.btnNhan.Click += new System.EventHandler(this.btnNhan_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(603, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Chia";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(776, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtSo1);
+            this.Controls.Add(this.txtSo2);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
