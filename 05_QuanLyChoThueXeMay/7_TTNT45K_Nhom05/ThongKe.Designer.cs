78da3d4f1bd57d1284af0b85a707ea4badab51f0
NgBaTruong <85273668+batruongnguyen@users.noreply.github.com>
2021-06-18 19:19:48 +0700

    Truong update giao dien

--- 05_QuanLyChoThueXeMay/7_TTNT45K_Nhom05/ThongKe.Designer.cs
+++ 05_QuanLyChoThueXeMay/7_TTNT45K_Nhom05/ThongKe.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmThongKe));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.cbTinhTrangXe = new System.Windows.Forms.ComboBox();
@@ -116,6 +117,7 @@
             this.Controls.Add(this.cbTinhTrangXe);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmThongKe";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Thống Kê";
